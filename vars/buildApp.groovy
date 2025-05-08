c540f59325608b879a050219b08b573fe9ed9907
spidey <spidey@spidey.spidey>
2025-05-09 00:19:03 +0300

    updating classes

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,6 +1,7 @@
 def call(Map config) {
     node {
-        Logger.info("Building ${config.appName} with version ${config.version}")
+        // Fixed: Added full package path
+        com.devops.utils.Logger.info("Building ${config.appName} with version ${config.version}")
         sh "mvn clean package -Dversion=${config.version}"
     }
 }
